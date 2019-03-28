87b3046c6be0d4f43d3741b0b97376b726983500
Osama Najjar <osama.najjar@mendix.com>
2019-03-28 17:44:12 +0100

    refactored to return free-of-popper AppSwitcher

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import Popper from 'popper.js';
 import './styles/styles.scss';
 
 export function MxAppSwitcher(options) {
-  let state, appSwitcherWrapperDiv, launchpadDiv, launchpadFrame, popperInstance, listenersHandlers;
+  let state, appSwitcherWrapperDiv, launchpadDiv, launchpadFrame, popperInstance, listenersHandlers, _isPopOver;
+
+  _isPopOver = options.toggleElement ? true : false;
   state = {
-    isOpen: false,
     initialized: false
   };
-
+  if (_isPopOver) {
+    state = { isOpen: false, ...state };
+  }
   //create the parent element of all
   appSwitcherWrapperDiv = document.createElement('div');
 
   //create the wrapper of the iframe which will load AppSwitcherService
   launchpadDiv = document.createElement('div');
   launchpadDiv.className = 'app-switcher__launchpad';
-
+  if (_isPopOver) launchpadDiv.classList.add('app-switcher__effect--shadow');
   //create the iframe & set the src url
   launchpadFrame = document.createElement('iframe');
   launchpadFrame.className = 'app-switcher__launchpad-frame';
+  if (!_isPopOver) launchpadFrame.classList.add('app-switcher__launchpad-frame--full-width');
   launchpadFrame.src = _getServiceURL();
 
   launchpadDiv.appendChild(launchpadFrame);
@@ -27,77 +31,93 @@ export function MxAppSwitcher(options) {
     return state;
   }
   function destroy() {
-    if (popperInstance && listenersHandlers) {
-      options.refNode.removeEventListener('click', listenersHandlers.toggleHandler);
-      document.removeEventListener('click', listenersHandlers.documentOnClickHandler);
+    if (listenersHandlers) {
+      if (_isPopOver) {
+        options.toggleElement.removeEventListener('click', listenersHandlers.toggleHandler);
+        document.removeEventListener('click', listenersHandlers.documentOnClickHandler);
+        popperInstance.destroy();
+      }
       window.removeEventListener('message', listenersHandlers.windowMessageHandler);
-      popperInstance.destroy();
     }
   }
   function init() {
     if (state.initialized) return;
-    if (!state.isOpen && !state.initialized) {
+    if (_isPopOver && !state.isOpen && !state.initialized) {
       appSwitcherWrapperDiv.classList.add('app-switcher__effect--hide');
     }
-    document.body.appendChild(appSwitcherWrapperDiv);
-    popperInstance = new Popper(options.refNode, appSwitcherWrapperDiv, {
-      placement: 'auto-end',
-      positionFixed: false,
-      eventsEnabled: true,
-      modifiers: {
-        offset: {
-          enabled: true,
-          offset: '0,10'
+    if (_isPopOver) {
+      document.body.appendChild(appSwitcherWrapperDiv);
+      popperInstance = new Popper(options.toggleElement, appSwitcherWrapperDiv, {
+        placement: 'auto-end',
+        positionFixed: false,
+        eventsEnabled: true,
+        modifiers: {
+          offset: {
+            enabled: true,
+            offset: '0,10'
+          },
+          flip: {
+            enabled: true
+          },
+          arrow: {
+            enabled: true
+          },
+          preventOverflow: {
+            boundariesElement: document.body
+          }
         },
-        flip: {
-          enabled: true
-        },
-        arrow: {
-          enabled: true
-        },
-        preventOverflow: {
-          boundariesElement: document.body
+        onCreate: function() {
+          state.initialized = true;
+          options.zIndex
+            ? (appSwitcherWrapperDiv.style.zIndex = options.zIndex)
+            : (appSwitcherWrapperDiv.style.zIndex = '1001');
+          _setEvent();
+          // call the provided on create callback
+          if (options.onCreate && typeof options.onCreate === 'function') options.onCreate();
         }
-      },
-      onCreate: function() {
-        state.initialized = true;
-        options.zIndex
-          ? (appSwitcherWrapperDiv.style.zIndex = options.zIndex)
-          : (appSwitcherWrapperDiv.style.zIndex = '1001');
-        _setEvent();
-        // call the provided on create callback
-        if (options.onCreate && typeof options.onCreate === 'function') options.onCreate();
-      }
-    });
-    return {
-      destroy: destroy,
-      getState: getState,
-      popperInstance: popperInstance,
-      wrappingDom: appSwitcherWrapperDiv
-    };
+      });
+      return {
+        destroy: destroy,
+        getState: getState,
+        popperInstance: popperInstance,
+        element: appSwitcherWrapperDiv
+      };
+    } else {
+      _setEvent();
+      // call the provided on create callback
+      if (options.onCreate && typeof options.onCreate === 'function') options.onCreate();
+      return {
+        destroy: destroy,
+        getState: getState,
+        element: appSwitcherWrapperDiv
+      };
+    }
   }
 
   //set toggling
   function _setEvent() {
     // register event listeners in this object for later removal on destroy
     listenersHandlers = {};
-
-    listenersHandlers.toggleHandler = event => {
-      event.stopPropagation();
-      if (!state.isOpen) {
-        _show();
-      } else {
-        _hide();
-      }
-      state.isOpen = !state.isOpen;
-    };
-
-    listenersHandlers.documentOnClickHandler = () => {
-      if (state.isOpen) {
-        _hide();
+    if (_isPopOver) {
+      listenersHandlers.toggleHandler = event => {
+        event.stopPropagation();
+        if (!state.isOpen) {
+          _show();
+        } else {
+          _hide();
+        }
         state.isOpen = !state.isOpen;
-      }
-    };
+      };
+
+      listenersHandlers.documentOnClickHandler = () => {
+        if (state.isOpen) {
+          _hide();
+          state.isOpen = !state.isOpen;
+        }
+      };
+      options.toggleElement.addEventListener('click', listenersHandlers.toggleHandler);
+      document.addEventListener('click', listenersHandlers.documentOnClickHandler);
+    }
     listenersHandlers.windowMessageHandler = function(messageEvent) {
       var origin = messageEvent.origin;
       //  make sure that the message is coming fromn a trusted origin
@@ -117,9 +137,6 @@ export function MxAppSwitcher(options) {
     };
 
     window.addEventListener('message', listenersHandlers.windowMessageHandler);
-
-    options.refNode.addEventListener('click', listenersHandlers.toggleHandler);
-    document.addEventListener('click', listenersHandlers.documentOnClickHandler);
   }
 
   function _show() {
