ce350497536ec96e16681eb98bd6494ac60b4697
Osama Najjar <osama.najjar@mendix.com>
2019-03-28 10:10:41 +0100

    moved webpack dev server assets to conf folder

--- conf/paths.js
+++ conf/paths.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  srcDir: path.join(__dirname, "..", "src"),
-  srcEntry: "./src/index.js",
+  srcDir: path.join(__dirname, '..', 'src'),
+  srcEntry: './src/index.js',
   confDir: __dirname,
-  distDir: path.join(__dirname, "..", "dist"),
-  publicDir: path.join(__dirname, "..", "public"),
+  distDir: path.join(__dirname, '..', 'dist'),
+  WebpackDevServerAssets: path.join(__dirname, 'WebpackDevServerAssets')
 };
--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,29 +1,41 @@
-const paths = require("./paths");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const fs = require("fs-extra");
+const paths = require('./paths');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const fs = require('fs-extra');
 
-const isDev = process.env.MODE === "development" ? true : false;
+const isDev = process.env.MODE === 'development' ? true : false;
 const MODES = {
-  DEV: "development",
-  PROD: "production",
+  DEV: 'development',
+  PROD: 'production'
 };
 
 module.exports = {
   mode: isDev ? MODES.DEV : MODES.PROD,
-  target: "web",
-  devtool: isDev ? "eval-source-map" : false,
+  target: 'web',
+  devtool: isDev ? 'eval-source-map' : false,
   watch: isDev,
   entry: paths.srcEntry,
   output: {
     path: paths.distDir,
-    filename: "mxappswitcher.js",
-    libraryTarget: "umd",
+    filename: 'mxappswitcher.js',
+    libraryTarget: 'umd'
   },
   devServer: {
-    contentBase: paths.publicDir,
+    contentBase: paths.WebpackDevServerAssets,
     port: 3000,
-    open: true,
+    open: true
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
   },
   module: {
     rules: [
@@ -31,35 +43,35 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          {loader: "postcss-loader", options: {config: {path: paths.confDir}}},
-          "sass-loader",
-        ],
+          'css-loader',
+          { loader: 'postcss-loader', options: { config: { path: paths.confDir } } },
+          'sass-loader'
+        ]
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: `[name].[ext]`,
-            },
-          },
-        ],
-      },
-    ],
+              name: `[name].[ext]`
+            }
+          }
+        ]
+      }
+    ]
   },
-  plugins: _getPlugins(),
+  plugins: _getPlugins()
 };
 
 function _getPlugins() {
@@ -67,8 +79,8 @@ function _getPlugins() {
   fs.ensureDirSync(paths.distDir);
   const plugins = [
     new MiniCssExtractPlugin({
-      filename: "mxappswitcher.css",
-    }),
+      filename: 'mxappswitcher.css'
+    })
     //new CopyPlugin([{from: paths.distDir, to: paths.publicDir}]),
   ];
 
